961b4c5e10523b2370055d21326d29c1d8140ab8
celestianguyen <celestianguyenx@gmail.com>
2026-04-06 14:58:45 +0800

    Exercise01 age calc

--- src/exercises/Exercise01.java
+++ src/exercises/Exercise01.java
@@ -1,7 +1,26 @@
 package exercises;
 
+import java.time.Year;
+import java.util.Scanner;
+
 public class Exercise01 {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your birth year: ");
+        int birthYear = scanner.nextInt();
+
+        int currentYear = Year.now().getValue();
+
+        int age;
+
+        if (currentYear < birthYear || birthYear <= 0) {
+            System.out.println("Invalid year");
+        } else {
+            age = currentYear - birthYear;
+            System.out.println("Your birth year is " + age);
+        }
+
     }
 }
